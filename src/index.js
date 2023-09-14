78d38ae1842c1e77f530898b8700175dcadaf2b8
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-14 10:28:58 -0400

    DONE: Refactored priority assignment

--- src/index.js
+++ src/index.js
@@ -5,28 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
     let todos = document.getElementById("tasks")
     let p = document.createElement('p')
     p.textContent = `${e.target["new-task-description"].value} `
-    if (e.target.priority.value === 'high'){
-      p.style.color = 'red'
-      todos = document.getElementById("highPriorityList")
-    } else if (e.target.priority.value === 'medium'){
-      p.style.color = 'blue'
-      todos = document.getElementById("mediumPriorityList")
-    } else {
-      p.style.color = 'green'
-      todos = document.getElementById("lowPriorityList")
-    }
+    let prioritySelected = e.target.priority.value
+    applyPriority(p,prioritySelected)
     let doneBtn = document.createElement('button')
     doneBtn.addEventListener('click', event => {
       let doneP = document.createElement('p')
       doneP.textContent = `${p.textContent}`
       document.querySelector("#tasksCompleted").append(doneP)
-
       event.target.parentNode.remove()
     })
     doneBtn.textContent = "Done!"
     p.appendChild(doneBtn)
     todos.append(p)
     form.reset()
+    
+    function applyPriority (element,priority){
+      if (priority === 'high'){
+        element.style.color = 'red'
+        todos = document.getElementById("highPriorityList")
+      } else if (e.target.priority.value === 'medium'){
+        element.style.color = 'blue'
+        todos = document.getElementById("mediumPriorityList")
+      } else {
+        element.style.color = 'green'
+        todos = document.getElementById("lowPriorityList")
+      }
+    }
 
 })
 
