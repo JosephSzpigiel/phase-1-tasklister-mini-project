0149e6d349576a52ea47ac1025a5f7c73c66c211
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-13 21:42:53 -0400

    DONE: Made Tasker with Priorities, Colors, and required fields

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    let todos = document.getElementById("tasks")
+    let p = document.createElement('p')
+    p.textContent = `${e.target["new-task-description"].value} `
+    if (e.target.priority.value === 'high'){
+      p.style.color = 'red'
+      todos = document.getElementById("highPriorityList")
+    } else if (e.target.priority.value === 'medium'){
+      p.style.color = 'blue'
+      todos = document.getElementById("mediumPriorityList")
+    } else {
+      p.style.color = 'green'
+      todos = document.getElementById("lowPriorityList")
+    }
+    let deleteBtn = document.createElement('button')
+    deleteBtn.addEventListener('click', event => {
+      event.target.parentNode.remove()
+    })
+    deleteBtn.textContent = "X"
+    p.appendChild(deleteBtn)
+    todos.append(p)
+    form.reset()
+
+})
+
 });
